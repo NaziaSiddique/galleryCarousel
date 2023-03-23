4f0b11a5b3c5808274139fd0190bbcd20e2a956e
NaziaSiddique <36114589+NaziaSiddique@users.noreply.github.com>
2023-03-23 08:53:04 +0000

    Arranging and grabbing teh images

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 let list = document.querySelector('.list');
 let imgs = Array.from(list.children);
-const nextBtn = document.querySelector('.btn-right');
+const nextBtn = document.querySelector(".btn-right");
+const prevBtn = document.querySelector(".btn-left");
+
+// Getting images width
+const imgWidth = imgs[0].getBoundingClientRect().width;
+console.log(imgWidth);
+
+// Arranging imgs next to each other
+function setImgPosition(img, index) {
+  img.style.left = imgWidth * index + "px";
+}
