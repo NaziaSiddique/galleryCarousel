cc623f580887f60c4e6779e38d6baee44b578115
NaziaSiddique <36114589+NaziaSiddique@users.noreply.github.com>
2023-03-23 09:02:16 +0000

    Making the next button dynamic

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const prevBtn = document.querySelector(".btn-left");
 
 // Getting images width
 const imgWidth = imgs[0].getBoundingClientRect().width;
-console.log(imgWidth);
+//console.log(imgWidth);
 
 // Arranging imgs next to each other
 function setImgPosition(img, index) {
@@ -13,3 +13,16 @@ function setImgPosition(img, index) {
 }
 
 imgs.forEach(setImgPosition);
+
+/*
+When right button is clicked, move images to the left.
+*/
+
+nextBtn.addEventListener('click', function () {
+  const currentImg = list.querySelector('.current-image');
+  const nextImg = currentImg.nextElementSibling;
+});
+
+/*
+When left button is clicked, move images to the right.
+*/
